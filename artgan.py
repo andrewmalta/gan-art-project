4a9dfcc04ce1cc3266718f54786ef9f5aee41f31
Andrew Malta <amalta@apple.com>
2020-07-02 23:39:35 -0700

    change path

--- artgan.py
+++ artgan.py
@@ -23,7 +23,7 @@ GENERATE_RES = 3
 IMAGE_SIZE = 128 # rows/cols
 IMAGE_CHANNELS = 3
 
-training_data = np.load('training_art.npy')
+training_data = np.load('futurism.npy')
 
 def build_discriminator(image_shape):
     model = Sequential()
